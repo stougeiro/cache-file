a8e177f07fc7aa7639258c98ac76ed1e53f1e2a8
STougeiro <sidneytougeiro@gmail.com>
2024-09-20 11:05:21 -0300

    ServiceProvider: Registering caching macro

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -20,11 +20,14 @@
             $this->app->singleton(CacheHandlerInterface::class, function() {
                 return new FileCacheHandler(CACHE);
             });
+
+            $this->app::macro('cache', function() {
+                return $this->app->make(CacheInterface::class);
+            });
         }
 
         public function boot(): void
         {
-            $this->app->cache = $this->app->make(CacheInterface::class);
         }
 
         public function terminate(): void
