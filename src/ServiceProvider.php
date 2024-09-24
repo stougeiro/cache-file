bfe2d31cd5f22689c6e0d3dba20e5545dcbd338f
STougeiro <sidneytougeiro@gmail.com>
2024-09-24 00:03:23 -0300

    Update dependencies Setting parameters with config function

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,13 +2,12 @@
 
     namespace STDW\Cache\File;
 
-    defined('CACHE') or define('CACHE', sys_get_temp_dir());
-
     use STDW\Contract\ServiceProviderAbstracted;
     use STDW\Cache\Contract\CacheInterface;
     use STDW\Cache\Contract\CacheHandlerInterface;
     use STDW\Cache\Cache;
     use STDW\Cache\File\FileCacheHandler;
+    use Throwable;
 
 
     class ServiceProvider extends ServiceProviderAbstracted
@@ -16,8 +15,17 @@
         public function register(): void
         {
             $this->app->singleton(CacheInterface::class, Cache::class);
-            $this->app->singleton(CacheHandlerInterface::class, function() {
-                return new FileCacheHandler(CACHE);
+            $this->app->singleton(CacheHandlerInterface::class, function()
+            {
+                try {
+                    $storage = config('cache.storage');
+                    $file_extension = config('cache.file_extension');
+                } catch (Throwable $e) {
+                    $storage = sys_get_temp_dir();
+                    $file_extension = '.cache';
+                }
+
+                return new FileCacheHandler($storage, $file_extension);
             });
         }
 
